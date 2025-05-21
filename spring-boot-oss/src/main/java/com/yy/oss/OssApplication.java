f46d25cb1a9c0fbaba3dccb972efada6bdf25c1f
15525438831 <15525438831@163.com>
2025-05-21 10:35:15 +0800

    配置

---
+++ spring-boot-oss/src/main/java/com/yy/oss/OssApplication.java
@@ -0,0 +1,19 @@
+package com.yy.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*********************************************************
+ ** 文件上传服务启动类
+ ** <br><br>
+ ** @ClassName: OssApplication
+ ** @author: yangfeng
+ ** @date: 2025/5/21 10:27
+ ** @version: 1.0.0
+ *********************************************************/
+@SpringBootApplication
+public class OssApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OssApplication.class, args);
+    }
+}
