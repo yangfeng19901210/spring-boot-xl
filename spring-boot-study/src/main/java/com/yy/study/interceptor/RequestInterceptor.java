71ceee3638896eba3c53bed7530bfe99d91c4c3d
15525438831 <15525438831@163.com>
2025-06-03 15:49:32 +0800

    异常信息完善

--- spring-boot-study/src/main/java/com/yy/study/interceptor/RequestInterceptor.java
+++ spring-boot-study/src/main/java/com/yy/study/interceptor/RequestInterceptor.java
@@ -21,13 +21,12 @@ import java.util.UUID;
 public class RequestInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String taskId = UUID.randomUUID().toString();
+        String taskId = UUID.randomUUID().toString().replace("-", "");
         BaseStorage.pushTaskId(taskId);
         return true;
     }
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println(2);
     }
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
